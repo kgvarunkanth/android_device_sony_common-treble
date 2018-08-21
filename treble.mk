d1d8b67abd8fe38c33fa5cdb62479d29173e6542
Christian Oder <myself5@carbonrom.org>
2018-08-21 22:02:01 +0200

    common: Use 1.1 NFC HAL
    Change-Id: I3998a84c3b3bd5cf06f9f3c95da0ea462f2837b0

--- treble.mk
+++ treble.mk
@@ -124,8 +124,8 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.0-impl \
-    android.hardware.nfc@1.0-service
+    android.hardware.nfc@1.1-impl \
+    android.hardware.nfc@1.1-service
 
 # GNSS
 #PRODUCT_PACKAGES += \
