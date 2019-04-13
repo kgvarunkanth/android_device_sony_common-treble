70667de2610dd5c832cff3c37ceb2bdfc39bb920
derfelot <frederic.koch@gmail.com>
2019-04-13 13:10:32 +0200

    [WIP] treble: Disable NFC and GNSS

--- treble.mk
+++ treble.mk
@@ -123,14 +123,14 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl
 
 # NFC
-PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.0-impl \
-    android.hardware.nfc@1.0-service
+#PRODUCT_PACKAGES += \
+#    android.hardware.nfc@1.0-impl \
+#    android.hardware.nfc@1.0-service
 
 # GNSS
-PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl-qti \
-    android.hardware.gnss@1.0-service-qti
+#PRODUCT_PACKAGES += \
+#    android.hardware.gnss@1.0-impl-qti \
+#    android.hardware.gnss@1.0-service-qti
 
 # LIGHT
 PRODUCT_PACKAGES += \
